4db3fd3c3694ac0d367d12e246169a05328c2f48
ashiq.shahansha <ashiq.shahansha@infosys.com>
2024-08-05 16:40:05 +0530

    adding all base files for parallel execution

---
+++ src/main/java/feature/parallelScenario.feature
@@ -0,0 +1,7 @@
+Feature: Scenarios feature file
+  @Ashiq
+  Scenario: Scenario Number One
+    Given Step from 'Scenario 1' in 'scenarios' feature file
+  @Ashiq
+  Scenario: Scenario Number Two
+    Given Step from 'Scenario 2' in 'scenarios' feature file
---
+++ src/main/java/parallel/RunCucumberTest.java
@@ -0,0 +1,19 @@
+package parallel;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src/main/java/feature/"}, // Path to your feature files
+        glue= {"parallel"}, // Path to your step definitions
+        tags="@Ashiq", // This will run the feature file tagged as @Ashiq
+        plugin= {"pretty","html:target/cucumberreport.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
+        monochrome=true
+        //,dryRun=true
+        //,strict=true
+        //,stepNotifications=true
+        //,snippets= CucumberOptions.SnippetType.CAMELCASE}
+        )
+public class RunCucumberTest {
+}
